58ee4f2eff3ea7938c814cbf6f5c181ff1a91a96
Igor <igor.carneiro13@hotmail.com>
2020-07-06 16:37:40 -0300

    Autenticação de admin.

---
+++ test/voting_web/controllers/admin/session_controller_test.exs
@@ -0,0 +1,30 @@
+defmodule VotingWeb.Admin.SessionControllerTest do
+  use VotingWeb.ConnCase, async: true
+
+  import Voting.Factory
+
+  describe "create/2" do
+    setup %{conn: conn} do
+      insert(:admin, name: "John Smith", email: "john@gmail.com")
+      
+      %{conn: conn}
+    end
+    
+    test "returns 200 when admin credentials are valids", %{conn: conn} do
+      conn = post(conn, "/api/v1/admin/sign_in", %{"email" => "john@gmail.com", "password" => "123456"})  
+
+      assert %{"status" => "ok", "data" => %{"name" => "John Smith"}} = json_response(conn, 200)
+    end
+
+    test "returns 401 when admin email is invalid", %{conn: conn} do
+      conn = post(conn, "/api/v1/admin/sign_in", %{"email" => "error@gmail.com", "password" => "123456"})  
+
+      assert %{"status" => "unauthenticated"} = json_response(conn, 401)
+    end
+    test "returns 401 when admin password is invalid", %{conn: conn} do
+      conn = post(conn, "/api/v1/admin/sign_in", %{"email" => "john@gmail.com", "password" => "asdf"})  
+
+      assert %{"status" => "unauthenticated"} = json_response(conn, 401)      
+    end
+  end
+end
---
+++ test/voting_web/views/admin/session_view_test.exs
@@ -0,0 +1,13 @@
+defmodule VotingWeb.Admin.SessionViewTest do
+  use ExUnit.Case, async: true
+
+  import Voting.Factory
+
+  alias VotingWeb.Admin.SessionView
+
+  test "render/2 returns ok and the admin data" do
+    admin = params_for(:admin, name: "John Smith") # Referente ao Voting.Factory.
+
+    assert %{status: "ok", data: %{name: "John Smith"}} = SessionView.render("session.json", %{admin: admin})
+  end
+end
